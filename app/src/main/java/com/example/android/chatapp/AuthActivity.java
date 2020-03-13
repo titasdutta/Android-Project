96d6bbaad36efa38b43dd4c0452f3adf95bbaffe
Titas Dutta <duttat@uci.edu>
2020-03-13 16:15:50 -0700

    Minor changes and sign in screen

--- app/src/main/java/com/example/android/chatapp/AuthActivity.java
+++ app/src/main/java/com/example/android/chatapp/AuthActivity.java
@@ -3,11 +3,39 @@ package com.example.android.chatapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+
 public class AuthActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        // Initialize Firebase Auth Object
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+
+
+    // Starting check if the user is already signed in
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if user is signed in and update the view
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+
+    private void updateUI(FirebaseUser curr) {}
 }
--- app/src/main/java/com/example/android/chatapp/MainActivity.java
+++ app/src/main/java/com/example/android/chatapp/MainActivity.java
@@ -9,41 +9,9 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        mAuth = FirebaseAuth.getInstance();
-    }
-
-    // Check to see if the user is currently signed in
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-    private void updateUI(FirebaseUser user) {
-//        hideProgressDialog();
-//        if (user != null) {
-//            mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
-//            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
-//
-//            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
-//        } else {
-//            mStatusTextView.setText(R.string.signed_out);
-//            mDetailTextView.setText(null);
-//
-//            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
-//        }
     }
 }
